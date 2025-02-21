f658fdb541f0cf22ec1bcf649dc62b3986b666d9
Guilherme.Felipe <guilherme.felipe@retta.com.br>
2025-02-21 08:50:22 -0300

    doc: iniciando a implementação da documentação.

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,12 @@ import {
   validatorCompiler,
   serializerCompiler,
   ZodTypeProvider,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
-import z from "zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+import { env } from "./env";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -16,26 +20,21 @@ app.register(fastifyCors, {
   origin: true,
 });
 
-app.post(
-  "/subscriptions",
-  {
-    schema: {
-      body: z.object({
-        name: z.string(),
-        email: z.string().email(),
-      }),
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect",
+      version: "0.0.1",
     },
   },
-  (request, reply) => {
-    const { name, email } = request.body;
+  transform: jsonSchemaTransform,
+});
 
-    return reply.status(201).send({
-      name,
-      email,
-    });
-  }
-);
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+app.register(subscribeToEventRoute);
 
-app.listen({ port: 3000 }, () => {
+app.listen({ port: env.PORT }, () => {
   console.log("Server is running ✅");
 });
